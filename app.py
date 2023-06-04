f15d27a27ee9fbd5206b8aaa20059e8d43fd80f9
wsb-pk <wrx78021@student.wsb.wroclaw.pl>
2023-06-04 17:24:51 +0200

    Aktualizacja app.py oraz requierements.txt

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-"""
-Docstring Piotr Kowalski
-"""
-from flask import Flask
+from flask import Flask, render_template
+from markupsafe import escape
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    """
-    Funkcja index
-    """
-    return '<h1>Hello WSB! Greetings from Flask!</h1>'
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
